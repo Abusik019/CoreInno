d26e2cbd1b73711b7aa62115da44691613777dab
lom-ali <lomali1999999@mail.ru>
2024-12-18 02:34:38 +0300

    registation

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,143 @@
+import React, { useState } from "react";
+
+import styles from "./style.module.css";
+
+import hidePasswordImg from "../../assets/hidePassword.svg";
+import showPasswordImg from "../../assets/showPassword.svg";
+import hidePasswordImgRed from "../../assets/redHidePassword.svg";
+import showPasswordImgRed from "../../assets/redShowPassword.svg";
+import vkImg from '../../assets/vk.svg';
+import googleImg from '../../assets/google.svg';
+import { useDispatch } from "react-redux";
+import { authSignIn } from "../../redux/slices/authSlice";
+import { Link } from "react-router-dom";
+
+function Login() {
+  const [hidePassword, setHidePassword] = useState(true);
+  const [emailVaildate, setEmailVaildate] = useState(true);
+  const [passwordVaildate, setPasswordVaildate] = useState(true);
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const dispatch = useDispatch()
+
+  function fetchSignIn (){
+    dispatch(authSignIn({email, password}))
+    setPassword('')
+    setEmail('')
+  }
+
+  function handleValidateEmail(e, changeState){
+    setEmail(e.target.value)
+    if(e.target.value !== ''){
+        const value = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e.target.value);
+        changeState(value);
+    } else{
+        changeState(true);
+    }
+    
+}
+
+function handleValidatePassword(e){
+  setPassword(e.target.value)
+  if (e.target.value.length < 8) {
+    setPasswordVaildate(false);
+  } else {
+    setPasswordVaildate(true);
+  }
+}
+
+  return (
+    <div className={styles.registration}>
+      <h1>Вход в аккаунт Giglink</h1>
+      <form className={styles.regForm}>
+        <div className={styles.emailWrapper}>
+          <label
+            style={{ color: emailVaildate ? "#000" : "#F63939" }}
+            htmlFor="email"
+          >
+            {emailVaildate ? "E-mail" : "E-mail введен некорректно"}
+          </label>
+          <input
+          value={email}
+            type="text"
+            id="email"
+            placeholder="Ваша почта"
+            onChange={(e) => handleValidateEmail(e, setEmailVaildate)}
+            style={{
+              borderColor: emailVaildate ? "#808080" : "#F63939",
+              color: emailVaildate ? "#000" : "#F63939",
+            }}
+            required
+          />
+        </div>
+        <div className={styles.passwordWrapper}>
+          <label
+            style={{ color: passwordVaildate ? "#000" : "#F63939" }}
+            htmlFor="password"
+          >
+            {passwordVaildate
+              ? "Пароль"
+              : "Пароль должен содержать минимум 8 символов"}
+          </label>
+          <input
+          value={password}
+            type={hidePassword ? "password" : "text"}
+            id="password"
+            placeholder="Ваш пароль"
+            onChange={(e) => handleValidatePassword(e) }
+            style={{
+              borderColor: passwordVaildate ? "#808080" : "#F63939",
+              color: passwordVaildate ? "#000" : "#F63939",
+            }}
+            required
+          />
+          <button
+            className={styles.passwordVisible}
+            onClick={(e) => {
+              e.preventDefault();
+              setHidePassword((prevState) => !prevState);
+            }}
+          >
+            <img
+              src={
+                hidePassword
+                  ? passwordVaildate
+                    ? showPasswordImg
+                    : showPasswordImgRed
+                  : passwordVaildate
+                  ? hidePasswordImg
+                  : hidePasswordImgRed
+              }
+            />
+          </button>
+        </div>
+        <button type="button" onClick={fetchSignIn}  className={styles.nextBtn}>
+          Продолжить
+        </button>
+        <label>
+          <input type="checkbox" required />Я согласен с условиями политики
+          конфиденциальности
+        </label>
+      </form>
+      <div className={styles.anotherRegistrations}>
+        <button>
+          <img src={vkImg} width={24} height={24} />
+          Войти через аккаунт Вконтакте
+        </button>
+        <button>
+          <img src={googleImg} width={24} height={24} />
+          Войти через аккаунт Google
+        </button>
+      </div>
+      <div className={styles.haveAccount}>
+        <a href="#">Нет аккаунта?</a>
+        <Link to='/registration'>Зарегистрироваться</Link>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Login;
--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -5,126 +5,210 @@ import hidePasswordImg from "../../assets/hidePassword.svg";
 import showPasswordImg from "../../assets/showPassword.svg";
 import hidePasswordImgRed from "../../assets/redHidePassword.svg";
 import showPasswordImgRed from "../../assets/redShowPassword.svg";
-import vkImg from '../../assets/vk.svg';
-import googleImg from '../../assets/google.svg';
+import vkImg from "../../assets/vk.svg";
+import googleImg from "../../assets/google.svg";
+import { useDispatch } from "react-redux";
+import { authSignUp } from "../../redux/slices/authSlice";
+import { Link } from "react-router-dom";
 
 export default function Registration() {
-    const [hidePassword, setHidePassword] = useState(true);
-    const [nameVaildate, setNameVaildate] = useState(true);
-    const [surnameVaildate, setSurnameVaildate] = useState(true);
-    const [emailVaildate, setEmailVaildate] = useState(true);
-    const [passwordVaildate, setPasswordVaildate] = useState(true);
+  const [hidePassword, setHidePassword] = useState(true);
+  const [nameVaildate, setNameVaildate] = useState(true);
+  const [surnameVaildate, setSurnameVaildate] = useState(true);
+  const [emailVaildate, setEmailVaildate] = useState(true);
+  const [passwordVaildate, setPasswordVaildate] = useState(true);
 
-    function handleValidateName(e, changeState){
-        console.log(e.target.value);
-        if(e.target.value !== ''){
-            const value = /^[a-zA-Zа-яА-я]+$/.test(e.target.value);
-            changeState(value);
-        } else{
-            changeState(true);
-        }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstname] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const dispatch = useDispatch();
+
+  function fetchSignUp() {
+    dispatch(authSignUp({ email, password, firstName, lastName }));
+    setEmail("");
+    setPassword("");
+    setFirstname("");
+    setLastName("");
+  }
+
+  function handleValidateName(e, changeState) {
+    setFirstname(e.target.value);
+    console.log(e.target.value);
+    if (e.target.value !== "") {
+      const value = /^[a-zA-Zа-яА-я]+$/.test(e.target.value);
+      changeState(value);
+    } else {
+      changeState(true);
     }
-
-    function handleValidateEmail(e, changeState){
-        console.log(e.target.value);
-        if(e.target.value !== ''){
-            const value = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e.target.value);
-            changeState(value);
-        } else{
-            changeState(true);
-        }
+  }
+  function handleValidateSurName(e, changeState) {
+    setLastName(e.target.value);
+    console.log(e.target.value);
+    if (e.target.value !== "") {
+      const value = /^[a-zA-Zа-яА-я]+$/.test(e.target.value);
+      changeState(value);
+    } else {
+      changeState(true);
     }
+  }
 
-    return (
-        <div className={styles.registration}>
-            <h1>Зарегистрируйтесь, чтобы найти исполнителя</h1>
-            <form className={styles.regForm}>
-                <div className={styles.formNameData}>
-                    <div>
-                        <label style={{color: nameVaildate ? '#000' : '#F63939'}} htmlFor="name">{nameVaildate ? 'Имя' : 'Имя введено некорректно'}</label>
-                        <input
-                            type="text"
-                            id="name"
-                            placeholder="Ваше имя"
-                            minLength={2}
-                            maxLength={30}
-                            onChange={(e) => handleValidateName(e, setNameVaildate)}
-                            style={{borderColor: nameVaildate ? '#808080' : '#F63939', color: nameVaildate ? '#000' : '#F63939'}}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label style={{color: surnameVaildate ? '#000' : '#F63939'}} htmlFor="surname">{surnameVaildate ? 'Фамилия' : 'Фамилия введена некорректно'}</label>
-                        <input
-                            type="text"
-                            id="surname"
-                            placeholder="Ваша фамилия"
-                            minLength={2}
-                            maxLength={30}
-                            onChange={(e) => handleValidateName(e, setSurnameVaildate)}
-                            style={{borderColor: surnameVaildate ? '#808080' : '#F63939', color: surnameVaildate ? '#000' : '#F63939'}}
-                            required
-                        />
-                    </div>
-                </div>
-                <div className={styles.emailWrapper}>
-                    <label style={{color: emailVaildate ? '#000' : '#F63939'}} htmlFor="email">{emailVaildate ? 'E-mail' : 'E-mail введен некорректно'}</label>
-                    <input
-                        type="text"
-                        id="email"
-                        placeholder="Ваша почта"
-                        onChange={(e) => handleValidateEmail(e, setEmailVaildate)}
-                        style={{borderColor: emailVaildate ? '#808080' : '#F63939', color: emailVaildate ? '#000' : '#F63939'}}
-                        required
-                    />
-                </div>
-                <div className={styles.passwordWrapper}>
-                    <label style={{color: passwordVaildate ? '#000' : '#F63939'}} htmlFor="password">{passwordVaildate ? "Пароль" : "Пароль должен содержать минимум 8 символов"}</label>
-                    <input
-                        type={hidePassword ? "password" : "text"}
-                        id="password"
-                        placeholder="Ваш пароль"
-                        onChange={(e) => {
-                            if(e.target.value.length < 8){
-                                setPasswordVaildate(false);
-                            } else{
-                                setPasswordVaildate(true);
-                            }
-                        }}
-                        style={{borderColor: passwordVaildate ? '#808080' : '#F63939', color: passwordVaildate ? '#000' : '#F63939'}}
-                        required
-                    />
-                    <button
-                        className={styles.passwordVisible}
-                        onClick={(e) => {
-                            e.preventDefault();
-                            setHidePassword((prevState) => !prevState);
-                        }}
-                    >
-                        <img
-                            src={
-                                hidePassword ? passwordVaildate ? showPasswordImg : showPasswordImgRed : passwordVaildate ? hidePasswordImg : hidePasswordImgRed
-                            }
-                        />
-                    </button>
-                </div>
-                <button className={styles.nextBtn}>Продолжить</button>
-                <label>
-                    <input type="checkbox" required/>
-                    Я согласен с условиями политики конфиденциальности
-                </label>
-            </form>
-            <div className={styles.anotherRegistrations}>
-                <button><img src={vkImg} width={24} height={24}/>Войти через аккаунт Вконтакте</button>
-                <button><img src={googleImg} width={24} height={24}/>Войти через аккаунт Google</button>
-            </div>
-            <div className={styles.haveAccount}>
-                <a href="#">Уже зарегистрированы?</a>
-                <a href="#">Войти в аккаунт</a>
-            </div>
-            <div className={styles.freelanceRegistration}>
-                <a href="#">Зарегистрироваться как фрилансер</a>
-            </div>
+  function handleValidateEmail(e, changeState) {
+    setEmail(e.target.value);
+    console.log(e.target.value);
+    if (e.target.value !== "") {
+      const value = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e.target.value);
+      changeState(value);
+    } else {
+      changeState(true);
+    }
+  }
+  function handleValidatePassword(e) {
+    setPassword(e.target.value);
+    if (e.target.value.length < 8) {
+      setPasswordVaildate(false);
+    } else {
+      setPasswordVaildate(true);
+    }
+  }
+
+  return (
+    <div className={styles.registration}>
+      <h1>Зарегистрируйтесь, чтобы найти исполнителя</h1>
+      <form className={styles.regForm}>
+        <div className={styles.formNameData}>
+          <div>
+            <label
+              style={{ color: nameVaildate ? "#000" : "#F63939" }}
+              htmlFor="name"
+            >
+              {nameVaildate ? "Имя" : "Имя введено некорректно"}
+            </label>
+            <input
+              value={firstName}
+              type="text"
+              id="name"
+              placeholder="Ваше имя"
+              minLength={2}
+              maxLength={30}
+              onChange={(e) => handleValidateName(e, setNameVaildate)}
+              style={{
+                borderColor: nameVaildate ? "#808080" : "#F63939",
+                color: nameVaildate ? "#000" : "#F63939",
+              }}
+              required
+            />
+          </div>
+          <div>
+            <label
+              style={{ color: surnameVaildate ? "#000" : "#F63939" }}
+              htmlFor="surname"
+            >
+              {surnameVaildate ? "Фамилия" : "Фамилия введена некорректно"}
+            </label>
+            <input
+              value={lastName}
+              type="text"
+              id="surname"
+              placeholder="Ваша фамилия"
+              minLength={2}
+              maxLength={30}
+              onChange={(e) => handleValidateSurName(e, setSurnameVaildate)}
+              style={{
+                borderColor: surnameVaildate ? "#808080" : "#F63939",
+                color: surnameVaildate ? "#000" : "#F63939",
+              }}
+              required
+            />
+          </div>
         </div>
-    );
+        <div className={styles.emailWrapper}>
+          <label
+            style={{ color: emailVaildate ? "#000" : "#F63939" }}
+            htmlFor="email"
+          >
+            {emailVaildate ? "E-mail" : "E-mail введен некорректно"}
+          </label>
+          <input
+            value={email}
+            type="text"
+            id="email"
+            placeholder="Ваша почта"
+            onChange={(e) => handleValidateEmail(e, setEmailVaildate)}
+            style={{
+              borderColor: emailVaildate ? "#808080" : "#F63939",
+              color: emailVaildate ? "#000" : "#F63939",
+            }}
+            required
+          />
+        </div>
+        <div className={styles.passwordWrapper}>
+          <label
+            style={{ color: passwordVaildate ? "#000" : "#F63939" }}
+            htmlFor="password"
+          >
+            {passwordVaildate
+              ? "Пароль"
+              : "Пароль должен содержать минимум 8 символов"}
+          </label>
+          <input
+            value={password}
+            type={hidePassword ? "password" : "text"}
+            id="password"
+            placeholder="Ваш пароль"
+            onChange={(e) => handleValidatePassword(e)}
+            style={{
+              borderColor: passwordVaildate ? "#808080" : "#F63939",
+              color: passwordVaildate ? "#000" : "#F63939",
+            }}
+            required
+          />
+          <button
+            className={styles.passwordVisible}
+            onClick={(e) => {
+              e.preventDefault();
+              setHidePassword((prevState) => !prevState);
+            }}
+          >
+            <img
+              src={
+                hidePassword
+                  ? passwordVaildate
+                    ? showPasswordImg
+                    : showPasswordImgRed
+                  : passwordVaildate
+                  ? hidePasswordImg
+                  : hidePasswordImgRed
+              }
+            />
+          </button>
+        </div>
+        <button type="button" onClick={fetchSignUp} className={styles.nextBtn}>
+          Продолжить
+        </button>
+        <label>
+          <input type="checkbox" required />Я согласен с условиями политики
+          конфиденциальности
+        </label>
+      </form>
+      <div className={styles.anotherRegistrations}>
+        <button>
+          <img src={vkImg} width={24} height={24} />
+          Войти через аккаунт Вконтакте
+        </button>
+        <button>
+          <img src={googleImg} width={24} height={24} />
+          Войти через аккаунт Google
+        </button>
+      </div>
+      <div className={styles.haveAccount}>
+        <a href="#">Уже зарегистрированы?</a>
+        <Link to="/login">Войти в аккаунт</Link>
+      </div>
+      <div className={styles.freelanceRegistration}>
+        <a href="#">Зарегистрироваться как фрилансер</a>
+      </div>
+    </div>
+  );
 }
