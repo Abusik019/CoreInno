f9c3e46b77fa0d426f0184f6f5d3d7eb79a9a1af
Абужапар Гасанбеков <abusikgasanbekov@MacBook-Air-Abuzapar.local>
2025-03-11 17:49:33 +0300

    finally create lending

--- src/pages/Lending/index.jsx
+++ src/pages/Lending/index.jsx
@@ -1,6 +1,7 @@
 import styles from './style.module.css';
 import { Navbar } from '../../components/Navbar';
 import { Link } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
 
 import bgImg from '../../assets/images/lending-bg-item.png';
 import videoImg from '../../assets/images/video.png';
@@ -12,133 +13,288 @@ import seoImg from '../../assets/icons/seo.svg';
 import designImg from '../../assets/icons/design.svg';
 import textsImg from '../../assets/icons/texts.svg';
 import linkArrowImg from '../../assets/icons/linkArrow.svg';
+import switch1Img from '../../assets/images/lendingSwitch1.png';
+import switch2Img from '../../assets/images/lendingSwitch2.png';
+import switch3Img from '../../assets/images/lendingSwitch3.png';
+import switch4Img from '../../assets/images/lendingSwitch4.png';
+import jobifyImg from '../../assets/icons/logoJobify.svg';
+import vkImg from '../../assets/icons/blackVK.svg';
+import tgImg from '../../assets/icons/blackTG.svg';
+import igImg from '../../assets/icons/blackIG.svg';
+
+const switchData = [
+    {
+        id: 1,
+        title: 'Новые функции платформы',
+        desc: 'Мы обновили биржу — добавили встроенный чат, быстрые переводы и систему гарантии сделок, а также поддержку пользователей',
+    },
+    {
+        id: 2,
+        title: 'Увеличение скорости выполнения заданий',
+        desc: 'Благодаря использованию нейросетей исполнители нашего сервиса смогут выполнить задания за пару часов, вместо того чтобы ждать 2-3 дня, как это делают конкуренты',
+    },
+    {
+        id: 3,
+        title: 'Адекватная ценовая политика',
+        desc: 'Комииссия сервиса будет значительно меньше, чем у конкурентов. Начинаться с 15% от суммы заказа и уменьшаться в зависимости от количества успешно выполненных работ',
+    },
+    {
+        id: 4,
+        title: 'Оптимизация логики формирования рейтинга',
+        desc: 'Формирование рейтинга будет базироваться на опыте человека, количестве качественных работ в портфолио, отзывах реальных клиентов',
+    },
+]
 
 export default function Lending() {
-  return (
-    <div className={styles.lending}>
-        <Navbar />
-        <main className={styles.main}> 
-            <div className={styles.mainInfo}>
-                <div className={styles.implementAI}>Внедряем ИИ</div>
-                <h2>Найдите идеальную работу или специалиста с Jobify!</h2>
-                <p>Платформа для поиска фриланс-заказов и исполнителей с удобным интерфейсом и надежными инструментами</p>
-                <div className={styles.mainBtns}>
-                    <button>Присоединиться</button>
-                    <button>Есть вопросы?</button>
-                </div>
-            </div>
-            <ul className={styles.advantages}>
-                <li>
-                    <h2><span>Удобный</span> сервис</h2>
-                    <p>Интуитивно понятно и ненавязчиво</p>
-                </li>
-                <li>
-                    <h2><span>Быстрый</span> поиск</h2>
-                    <p>Поиск клиентов и исполнителей за считаные минуты</p>
-                </li>
-                <li>
-                    <h2>У нас нет подписок</h2>
-                    <p>Рейтинг зависит от портфолио и успешно выполненных заказов</p>
-                </li>
-                <li>
-                    <h2><span>Защита</span> всех сделок</h2>
-                    <p>Мы уделили особое внимание вашей безопасности</p>
-                </li>
-            </ul>
-        </main>
-        <section className={styles.steps}>
-            <img src={bgImg} alt="bg item" />
-            <div className={styles.stepsContent}>
-                <div className={styles.stepsSwitchRole}>
-                    <h2>Как мне находить исполнителей?</h2>
-                    <div>
-                        <button>Я фрилансер</button>
-                        <button>Я заказчик</button>
+    const [activeItem, setActiveItem] = useState(null);
+    const sectionRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true);
+                }
+            },
+            { threshold: 0.3 } 
+        );
+
+        if (sectionRef.current) {
+            observer.observe(sectionRef.current);
+        }
+
+        return () => {
+            if (sectionRef.current) {
+                observer.unobserve(sectionRef.current);
+            }
+        };
+    }, []);
+
+    useEffect(() => {
+        if (isVisible) {
+            setActiveItem(1)
+        }
+    }, [isVisible]);
+
+    useEffect(() => {
+        if (isVisible && activeItem !== null) {
+            const timer = setTimeout(() => {
+                setActiveItem((prev) => (prev !== 4 ? prev + 1 : null));
+            }, 5000);
+    
+            return () => clearTimeout(timer);
+        }
+    }, [isVisible, activeItem]);
+
+    function changeImageSource(){
+        switch(activeItem){
+            case 1:
+                return switch1Img;
+            case 2:
+                return switch2Img;
+            case 3:
+                return switch3Img;
+            case 4:
+                return switch4Img;
+            default:
+                return switch1Img;
+        }
+    }
+
+    return (
+        <div className={styles.lending}>
+            <Navbar />
+            <main className={styles.main}> 
+                <div className={styles.mainInfo}>
+                    <div className={styles.implementAI}>Внедряем ИИ</div>
+                    <h2>Найдите идеальную работу или специалиста с Jobify!</h2>
+                    <p>Платформа для поиска фриланс-заказов и исполнителей с удобным интерфейсом и надежными инструментами</p>
+                    <div className={styles.mainBtns}>
+                        <button>Присоединиться</button>
+                        <button>Есть вопросы?</button>
                     </div>
                 </div>
-                <ul className={styles.stepsList}>
+                <ul className={styles.advantages}>
                     <li>
-                        <h2>Шаг №1</h2>
-                        <p>Создайте задание/Найдите подходящий проект</p>
+                        <h2><span>Удобный</span> сервис</h2>
+                        <p>Интуитивно понятно и ненавязчиво</p>
                     </li>
                     <li>
-                        <h2>Шаг №2</h2>
-                        <p>Выберите исполнителя/Откликнитесь на заказ</p>
+                        <h2><span>Быстрый</span> поиск</h2>
+                        <p>Поиск клиентов и исполнителей за считаные минуты</p>
                     </li>
                     <li>
-                        <h2>Шаг №3</h2>
-                        <p>Оплачивайте через безопасную сделку и получайте безупречный результат/Работайте и получайте оплату через безопасную сделку</p>
+                        <h2>У нас нет подписок</h2>
+                        <p>Рейтинг зависит от портфолио и успешно выполненных заказов</p>
+                    </li>
+                    <li>
+                        <h2><span>Защита</span> всех сделок</h2>
+                        <p>Мы уделили особое внимание вашей безопасности</p>
                     </li>
                 </ul>
-            </div>
-        </section>
-        <section className={styles.tutorial}>
-            <div className={styles.tutorialContent}>
-                <div className={styles.implementAI}>Что-то тут будет</div>
-                <h2>Туториал по работе на Jobify</h2>
-                <p>Мы обновили биржу — добавили встроенный чат, быстрые переводы и систему гарантии сделок, а также поддержку пользователей</p>
-                <button>Присоединиться</button>
-            </div>
-            <img 
-                src={videoImg}
-                width={652}
-                height={410}
-                alt='video'
-            />
-        </section>
-        <section className={styles.links}>
-            <ul className={styles.linksList}>
-                <li>
-                    <h2>Разработка и IT</h2>
-                    <div>
-                        <img src={itImg} width={78} height={78} alt="it" />
-                        <Link to="#"><img src={linkArrowImg} width={24} height={24} alt="link" /></Link>
+            </main>
+            <section className={styles.steps}>
+                <img src={bgImg} alt="bg item" />
+                <div className={styles.stepsContent}>
+                    <div className={styles.stepsSwitchRole}>
+                        <h2>Как мне находить исполнителей?</h2>
+                        <div>
+                            <button>Я фрилансер</button>
+                            <button>Я заказчик</button>
+                        </div>
                     </div>
-                </li>
-                <li>
-                    <h2>SMM и реклама</h2>
-                    <div>
-                        <img src={smmImg} width={92} height={68} alt="smm" />
-                        <Link to="#"><img src={linkArrowImg} width={24} height={24} alt="link" /></Link>
+                    <ul className={styles.stepsList}>
+                        <li>
+                            <h2>Шаг №1</h2>
+                            <p>Создайте задание/Найдите подходящий проект</p>
+                        </li>
+                        <li>
+                            <h2>Шаг №2</h2>
+                            <p>Выберите исполнителя/Откликнитесь на заказ</p>
+                        </li>
+                        <li>
+                            <h2>Шаг №3</h2>
+                            <p>Оплачивайте через безопасную сделку и получайте безупречный результат/Работайте и получайте оплату через безопасную сделку</p>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <section className={styles.tutorial}>
+                <div className={styles.tutorialContent}>
+                    <div className={styles.implementAI}>Что-то тут будет</div>
+                    <h2>Туториал по работе на Jobify</h2>
+                    <p>Мы обновили биржу — добавили встроенный чат, быстрые переводы и систему гарантии сделок, а также поддержку пользователей</p>
+                    <button>Присоединиться</button>
+                </div>
+                <img 
+                    src={videoImg}
+                    width={652}
+                    height={410}
+                    alt='video'
+                />
+            </section>
+            <section className={styles.links}>
+                <ul className={styles.linksList}>
+                    <li>
+                        <h2>Разработка и IT</h2>
+                        <div>
+                            <img src={itImg} width={78} height={78} alt="it" />
+                            <Link to="#"><img src={linkArrowImg} width={24} height={24} alt="link" /></Link>
+                        </div>
+                    </li>
+                    <li>
+                        <h2>SMM и реклама</h2>
+                        <div>
+                            <img src={smmImg} width={92} height={68} alt="smm" />
+                            <Link to="#"><img src={linkArrowImg} width={24} height={24} alt="link" /></Link>
+                        </div>
+                    </li>
+                    <li>
+                        <h2>Видео и аудио</h2>
+                        <div>
+                            <img src={audioImg} width={100} height={100} alt="video" />
+                            <Link to="#"><img src={linkArrowImg} width={24} height={24} alt="link" /></Link>
+                        </div>
+                    </li>
+                    <li>
+                        <h2>Бизнес и обучение</h2>
+                        <div>
+                            <img src={businessImg} width={97} height={79} alt="business" />
+                            <Link to="#"><img src={linkArrowImg} width={24} height={24} alt="link" /></Link>
+                        </div>
+                    </li>
+                    <li>
+                        <h2>SEO и аналитика</h2>
+                        <div>
+                            <img src={seoImg} width={100} height={100} alt="seo" style={{opacity: '.1'}}/>
+                            <Link to="#"><img src={linkArrowImg} width={24} height={24} alt="link" /></Link>
+                        </div>
+                    </li>
+                    <li>
+                        <h2>Дизайн</h2>
+                        <div>
+                            <img src={designImg} width={100} height={100} alt="design" />
+                            <Link to="#"><img src={linkArrowImg} width={24} height={24} alt="link" /></Link>
+                        </div>
+                    </li>
+                    <li>
+                        <h2>Тексты и переводы</h2>
+                        <div>
+                            <img src={textsImg} width={100} height={100} alt="texts" />
+                            <Link to="#"><img src={linkArrowImg} width={24} height={24} alt="link" /></Link>
+                        </div>
+                    </li>
+                </ul>
+            </section>
+            <section ref={sectionRef} className={styles.switchInfo}>
+                <img src={changeImageSource()} width={474} height={531} alt="switch" />
+                <ul className={styles.switchInfoList}>
+                    {switchData.map(item => (
+                        <li key={item.id} className={`${activeItem === item.id ? styles.active : ''}`}>
+                            <div onClick={() => setActiveItem(item.id)}></div>
+                            <div>
+                                <h2>{item.title}</h2>
+                                <p>{item.desc}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+            <section className={styles.stats}>
+                <ul className={styles.statsList}>
+                    <li>
+                        <h2>1000+</h2>
+                        <p>Выполненных заказов</p>
+                    </li>
+                    <li>
+                        <h2>100К+</h2>
+                        <p>Посещений в месяц</p>
+                    </li>
+                    <li>
+                        <h2>15К+</h2>
+                        <p>Активных пользователей</p>
+                    </li>
+                    <li>
+                        <h2>99.9%</h2>
+                        <p>Успешных заказов</p>
+                    </li>
+                </ul>
+            </section>
+            <section className={styles.course}>
+                <h2>Курс по искусственному интелекту</h2>
+                <p>Наш курс по искусственному интеллекту (ИИ) создан специально для фрилансеров, которые хотят зарабатывать больше и автоматизировать рутинные задачи.</p>
+                <button>Перейти к курсу</button>
+            </section>
+            <section className={styles.getFirstOffer}>
+                <h2><span>Ищешь работу?</span> Возьми свой первый заказ!</h2>
+                <p>Начните работать с Jobify уже сегодня!</p>
+                <input type="text" placeholder='Что будем искать?'/>
+                <button>Найти работу</button>
+            </section>
+            <footer className={styles.footer}>
+                <div className={styles.footerTop}>
+                    <div className={styles.footerLogoBlock}>
+                        <img src={jobifyImg} width={119} height={39} alt="Jobify logo" />
+                        <h2>Будущее начинается с тебя!</h2>
                     </div>
-                </li>
-                <li>
-                    <h2>Видео и аудио</h2>
-                    <div>
-                        <img src={audioImg} width={100} height={100} alt="video" />
-                        <Link to="#"><img src={linkArrowImg} width={24} height={24} alt="link" /></Link>
+                    <div className={styles.foooterLinks}>
+                        <div>
+                            <Link to="#"><img src={vkImg} width={24} height={24} alt="vk" /></Link>
+                            <Link to="#"><img src={tgImg} width={24} height={24} alt="tg" /></Link>
+                            <Link to="#"><img src={igImg} width={24} height={24} alt="ig" /></Link>
+                        </div>
+                        <h2>core.inno@mail.ru</h2>
                     </div>
-                </li>
-                <li>
-                    <h2>Бизнес и обучение</h2>
+                </div>
+                <div className={styles.footerBottom}>
+                    <h2>© Jobify 2025 — Все права защищены</h2>
                     <div>
-                        <img src={businessImg} width={97} height={79} alt="business" />
-                        <Link to="#"><img src={linkArrowImg} width={24} height={24} alt="link" /></Link>
+                        <Link to="#">Пользовательское соглашение</Link>
+                        <Link to="#">Политика конфиденциальности</Link>
                     </div>
-                </li>
-                <li>
-                    <h2>SEO и аналитика</h2>
-                    <div>
-                        <img src={seoImg} width={100} height={100} alt="seo" style={{opacity: '.1'}}/>
-                        <Link to="#"><img src={linkArrowImg} width={24} height={24} alt="link" /></Link>
-                    </div>
-                </li>
-                <li>
-                    <h2>Дизайн</h2>
-                    <div>
-                        <img src={designImg} width={100} height={100} alt="design" />
-                        <Link to="#"><img src={linkArrowImg} width={24} height={24} alt="link" /></Link>
-                    </div>
-                </li>
-                <li>
-                    <h2>Тексты и переводы</h2>
-                    <div>
-                        <img src={textsImg} width={100} height={100} alt="texts" />
-                        <Link to="#"><img src={linkArrowImg} width={24} height={24} alt="link" /></Link>
-                    </div>
-                </li>
-            </ul>
-        </section>
-    </div>
-  )
+                </div>
+            </footer>
+        </div>
+    )
 }
